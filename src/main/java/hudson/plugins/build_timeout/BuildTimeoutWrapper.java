5622e33f73ee840fef8f9967624395d9b69ce425
unknown <dudug@.kenshootlv.local>
2011-09-21 16:37:02 +0300

    new functionality - calc average build time for timeout enforcemen

--- src/main/java/hudson/plugins/build_timeout/BuildTimeoutWrapper.java
+++ src/main/java/hudson/plugins/build_timeout/BuildTimeoutWrapper.java
@@ -2,89 +2,186 @@ package hudson.plugins.build_timeout;
 
 import hudson.Extension;
 import hudson.Launcher;
-import hudson.model.AbstractBuild;
-import hudson.model.AbstractProject;
-import hudson.model.BuildListener;
-import hudson.model.Descriptor;
-import hudson.model.Executor;
-import hudson.model.Result;
+import hudson.model.*;
 import hudson.tasks.BuildWrapper;
 import hudson.tasks.BuildWrapperDescriptor;
 import hudson.triggers.SafeTimerTask;
 import hudson.triggers.Trigger;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 import java.io.IOException;
 
-import org.kohsuke.stapler.DataBoundConstructor;
-
 /**
- * {@link BuildWrapper} that terminates a build if it's taking too long.
+ * {@link BuildWrapper}
+ * that terminates a build if it's taking too long, in comparisson to previous builds
  *
- * @author Kohsuke Kawaguchi
+ * @author gold.dudu@gmail.com [modified Kohsuke Kawaguchi's, build-timeout]
  */
 public class BuildTimeoutWrapper extends BuildWrapper {
+
+    private int delaySecondsForTest = 0;
+
     /**
-     * If the build took longer than this amount of minutes,
-     * it will be terminated.
+     * If the build took longer than this % over the average calculated: timeout
      */
-    public int timeoutMinutes;
+    public int thresholdPercentage = 30;
+
+    private int timeoutMinutesCalculated;
 
     /**
      * Fail the build rather than aborting it
      */
     public boolean failBuild;
-    
+
+    /**
+     * number of builds to calculate average on
+     */
+    public int buildsToCalculateAverage = 3;
+
+    /**
+     * hard coded timeout in seconds to override average timeout
+     */
+    public int timeoutSeconds = -1;
+
     @DataBoundConstructor
-    public BuildTimeoutWrapper(int timeoutMinutes, boolean failBuild) {
-        this.timeoutMinutes = Math.max(3,timeoutMinutes);
+    public BuildTimeoutWrapper(int thresholdPercentage, int buildsToCalculateAverage, boolean failBuild, int timeoutSeconds) {
+        /**
+         * thresholdPercentage must be bigger than 10%
+         */
+        this.thresholdPercentage = Math.max(10, thresholdPercentage);
+        /**
+         * buildsToCalculateAverage must be bigger than 3
+         */
+        this.buildsToCalculateAverage = Math.max(3, buildsToCalculateAverage);
+
         this.failBuild = failBuild;
+
+        this.timeoutSeconds = timeoutSeconds;
     }
-    
+
     @Override
     public Environment setUp(final AbstractBuild build, Launcher launcher, final BuildListener listener) throws IOException, InterruptedException {
         class EnvironmentImpl extends Environment {
             final class TimeoutTimerTask extends SafeTimerTask {
+
                 private final AbstractBuild build;
                 private final BuildListener listener;
                 //Did the task timeout?
-                public boolean timeout= false;
+                public boolean timeout = false;
+
 
                 private TimeoutTimerTask(AbstractBuild build, BuildListener listener) {
                     this.build = build;
                     this.listener = listener;
+                    int average = getPreviousBuildsTimeAverage(build);
+                    int threshold = thresholdPercentage;
+//                    System.out.println("Threshold % input = " + threshold);
+//                    System.out.println("average = " + average);
+                    timeoutMinutesCalculated = average + (threshold / 100) * average;
+//                    System.out.println("timeout set to = " + timeoutMinutesCalculated);
                 }
 
                 public void doRun() {
+
+                    String msg = "Build time exceeded ( " + timeoutMinutesCalculated + " seconds)," +
+                            " that is the average of previous passed builds." +
+                            " Marking the build as ";
+
                     // timed out
-                    if (failBuild)
-                        listener.getLogger().println("Build timed out (after " + timeoutMinutes + " minutes). Marking the build as failed.");
-                    else
-                        listener.getLogger().println("Build timed out (after " + timeoutMinutes + " minutes). Marking the build as aborted.");
-                    timeout=true;
+                    listener.getLogger().println(msg + (failBuild ? "failed." : "unstable."));
+
+                    timeout = true;
                     Executor e = build.getExecutor();
-                    if (e != null)
-                        e.interrupt(failBuild? Result.FAILURE : Result.ABORTED);
+                    if (e != null) {
+                        if (failBuild) {
+                            listener.getLogger().println("Stopping the build");
+                            e.interrupt(Result.FAILURE);
+                        } else {
+                            listener.getLogger().println("Letting the build finish");
+                            build.setResult(Result.UNSTABLE);
+                        }
+                    }
+                }
+
+                /**
+                 * gets builds time [if not found return -1]
+                 * @return time int
+                 */
+                private int getLastSuccessfulBuildTime(Run run) {
+
+                    long timeMiliSecs = run.getDuration();
+                    long timeSecs = (timeMiliSecs / 1000);
+                    int buildTime = (int) timeSecs;
+//                    System.out.println("run time [sec] = " + buildTime);
+                    return buildTime;
+                }
+
+                /**
+                 * gets 3 previous success builds time average
+                 * @return time int
+                 */
+                public int getPreviousBuildsTimeAverage(AbstractBuild build) {
+                    System.out.println("getting previous " + buildsToCalculateAverage + " builds time");
+                    int lastThreeRunsTime = 0;
+                    Run run = build;
+                    for (int i = 0; i < buildsToCalculateAverage; i++) {
+                        try {
+                            run = run.getPreviousSuccessfulBuild();
+                            run.getNumber();
+//                            System.out.println("run number = " + run.getNumber());
+                        } catch (Exception e) {
+//                            System.out.println("Did not find successful build");
+                            return -1;
+                        }
+                        lastThreeRunsTime += getLastSuccessfulBuildTime(run);
+                    }
+//                    System.out.println("total " + buildsToCalculateAverage + " builds = " + lastThreeRunsTime);
+                    return lastThreeRunsTime > 0 ? lastThreeRunsTime / buildsToCalculateAverage : -1;
                 }
             }
 
             private final TimeoutTimerTask task;
-            
+
             public EnvironmentImpl() {
                 task = new TimeoutTimerTask(build, listener);
-                Trigger.timer.schedule(task, timeoutMinutes*60L*1000L );
+
+                if (timeoutMinutesCalculated > 0 || timeoutSeconds > 0) {
+
+                    int timeOutToUse = timeoutMinutesCalculated;
+
+                    /**
+                     * get the minimum valid timeout to enforce
+                     */
+                    if (timeoutMinutesCalculated > 0 && timeoutSeconds > 0)
+                        timeOutToUse = Math.min(timeoutSeconds, timeoutMinutesCalculated);
+                    else if (timeoutSeconds > 0)
+                        timeOutToUse = timeoutSeconds;
+                    listener.getLogger().println("Setting timeout for " + timeOutToUse + " previous success builds average + threshold [" + thresholdPercentage + "%] : " + timeoutMinutesCalculated);
+                    Trigger.timer.schedule(task, timeOutToUse * 1000L);
+                } else {
+//                    System.out.println("Was not able to find previous " + buildsToCalculateAverage + " success builds so no timeout is enforced");
+                }
+                if (delaySecondsForTest > 0) {
+                    try {
+                        System.out.println("sleep for " + delaySecondsForTest + " Secs ");
+                        Thread.sleep(delaySecondsForTest * 1000);
+                    } catch (InterruptedException e) {
+                        //nothing to do
+                    }
+                }
             }
 
             @Override
             public boolean tearDown(AbstractBuild build, BuildListener listener) throws IOException, InterruptedException {
                 task.cancel();
-                return (!task.timeout ||!failBuild);
+                return (!task.timeout || !failBuild);
             }
         }
 
         return new EnvironmentImpl();
     }
 
-    
+
     @Override
     public Descriptor<BuildWrapper> getDescriptor() {
         return DESCRIPTOR;
@@ -106,5 +203,9 @@ public class BuildTimeoutWrapper extends BuildWrapper {
             return true;
         }
 
+    }
+
+    public void setDelaySecondsForTest(int delaySecondsForTest) {
+        this.delaySecondsForTest = delaySecondsForTest;
     }
 }
--- src/test/java/hudson/plugins/build_timeout/BuildTimeoutWrapperIntegrationTest.java
+++ src/test/java/hudson/plugins/build_timeout/BuildTimeoutWrapperIntegrationTest.java
@@ -3,25 +3,73 @@ package hudson.plugins.build_timeout;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Result;
-import hudson.tasks.BuildWrapper;
-import hudson.tasks.test.AbstractTestResultAction;
-
 import org.jvnet.hudson.test.Bug;
 import org.jvnet.hudson.test.HudsonTestCase;
 import org.jvnet.hudson.test.recipes.LocalData;
 
+/**
+ * @author gold.dudu@gmail.com
+ */
 public class BuildTimeoutWrapperIntegrationTest extends HudsonTestCase {
 
-	@Bug(9203)
-	@LocalData
-	public void testIssue9203() throws Exception {
-		FreeStyleProject project = (FreeStyleProject) hudson.getItem("9203");
-		
-		// Force a timeout of 1, plugin will not accept less than 3 minutes.
-		BuildTimeoutWrapper buildWrapper = (BuildTimeoutWrapper) project.getBuildWrappersList().get(0);
-		buildWrapper.timeoutMinutes = 1;
-		
-		FreeStyleBuild build = project.scheduleBuild2(0).get();
-		assertBuildStatus(Result.FAILURE, build);
-	}
+    @Bug(9203)
+    @LocalData
+    public void testIssue9203() throws Exception {
+        FreeStyleProject project = (FreeStyleProject) hudson.getItem("9203");
+
+        BuildTimeoutWrapper buildWrapper = (BuildTimeoutWrapper) project.getBuildWrappersList().get(0);
+        buildWrapper.thresholdPercentage = 100;
+        buildWrapper.buildsToCalculateAverage = 3;
+        buildWrapper.failBuild = true;
+        buildWrapper.setDelaySecondsForTest(1);
+
+        //build 1
+        System.out.println("toFail = true, build to average = 3, threshold = 100, hard-coded = 0, no average");
+        FreeStyleBuild build = project.scheduleBuild2(0).get();
+        assertBuildStatus(Result.SUCCESS, build);
+
+        //build 2
+        System.out.println("toFail = true, build to average = 3, threshold = 100, hard-coded = 0, no average");
+        build = project.scheduleBuild2(0).get();
+        assertBuildStatus(Result.SUCCESS, build);
+
+        //build 3
+        System.out.println("toFail = true, build to average = 3, threshold = 100, hard-coded = 0, no average");
+        build = project.scheduleBuild2(0).get();
+        assertBuildStatus(Result.SUCCESS, build);
+
+        //build 4
+        System.out.println("toFail = true, build to average = 3, threshold = 100, hard-coded = 0, average = 1");
+        build = project.scheduleBuild2(0).get();
+        assertBuildStatus(Result.SUCCESS, build);
+
+        //build 5
+        System.out.println("toFail = true, build to average = 3, threshold = 100, hard-coded = 0, average = 1, build time = 3");
+        buildWrapper.setDelaySecondsForTest(3);
+        build = project.scheduleBuild2(0).get();
+        assertBuildStatus(Result.FAILURE, build);
+
+
+        //build 6
+        System.out.println("toFail = false, build to average = 3, threshold = 100, hard-coded = 0, average = 2, build time = 3");
+        buildWrapper.failBuild = false;
+        buildWrapper.setDelaySecondsForTest(3);
+        build = project.scheduleBuild2(0).get();
+        assertBuildStatus(Result.UNSTABLE, build);
+
+        //build 7
+        System.out.println("toFail = false, build to average = 3, threshold = 300, hard-coded = 0, average = 2, build time = 2");
+        buildWrapper.thresholdPercentage = 300;
+        buildWrapper.setDelaySecondsForTest(2);
+        build = project.scheduleBuild2(0).get();
+        assertBuildStatus(Result.SUCCESS, build);
+
+        //build 8
+        System.out.println("toFail = false, build to average = 3, threshold = 100, hard-coded = 1, average = 2, build time = 2");
+        buildWrapper.timeoutSeconds = 1;
+        buildWrapper.setDelaySecondsForTest(2);
+        build = project.scheduleBuild2(0).get();
+        assertBuildStatus(Result.UNSTABLE, build);
+
+    }
 }
