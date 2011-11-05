0083f6fda94b9357ef70674b9513214a93c4515a
ikikko <ikikko+github@gmail.com>
2011-11-05 12:02:34 +0900

    add flag to select timeout strategy

--- src/main/java/hudson/plugins/build_timeout/BuildTimeoutWrapper.java
+++ src/main/java/hudson/plugins/build_timeout/BuildTimeoutWrapper.java
@@ -18,7 +18,10 @@ import hudson.util.TimeUnit2;
 
 import java.io.IOException;
 
+import net.sf.json.JSONObject;
+
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
 
 /**
  * {@link BuildWrapper} that terminates a build if it's taking too long.
@@ -26,6 +29,17 @@ import org.kohsuke.stapler.DataBoundConstructor;
  * @author Kohsuke Kawaguchi
  */
 public class BuildTimeoutWrapper extends BuildWrapper {
+
+    /**
+     * Timeout occurs after set time.
+     */
+    public boolean fixationTimeStrategy;
+
+    /**
+     * Timeout occurs if the build is likely stuck.
+     */
+    public boolean likelyStuckStrategy;
+
     /**
      * If the build took longer than this amount of minutes,
      * it will be terminated.
@@ -38,7 +52,10 @@ public class BuildTimeoutWrapper extends BuildWrapper {
     public boolean failBuild;
 
     @DataBoundConstructor
-    public BuildTimeoutWrapper(int timeoutMinutes, boolean failBuild) {
+    public BuildTimeoutWrapper(boolean fixationTimeStrategy, boolean likelyStuckStrategy, int timeoutMinutes,
+            boolean failBuild) {
+        this.fixationTimeStrategy = fixationTimeStrategy;
+        this.likelyStuckStrategy = likelyStuckStrategy;
         this.timeoutMinutes = Math.max(3,timeoutMinutes);
         this.failBuild = failBuild;
     }
@@ -71,44 +88,45 @@ public class BuildTimeoutWrapper extends BuildWrapper {
             }
 
             private final TimeoutTimerTask task;
-            
 
             public EnvironmentImpl() {
-				long timeout;
-				if (true) {
-					timeout = getLikelyStuckTime();
-				} else {
-					timeout = timeoutMinutes * 60L * 1000L;
-				}
+                long timeout;
+                if (fixationTimeStrategy) {
+                    timeout = timeoutMinutes * 60L * 1000L;
+                } else if (likelyStuckStrategy) {
+                    timeout = getLikelyStuckTime();
+                } else {
+                    throw new IllegalStateException("timeout strategy is not selected.");
+                }
 
-				task = new TimeoutTimerTask(build, listener);
-				Trigger.timer.schedule(task, timeout);
-			}
+                task = new TimeoutTimerTask(build, listener);
+                Trigger.timer.schedule(task, timeout);
+            }
 
-			/**
-			 * Get the time considered it stuck.
-			 * 
-			 * @return 10 times as much as eta if eta is available, else 24 hours.
-			 * @see Executor#isLikelyStuck()
-			 */
-			private long getLikelyStuckTime() {
-				Executor executor = build.getExecutor();
-				if (executor == null) {
-					return TimeUnit2.HOURS.toMillis(24);
-				}
+            /**
+             * Get the time considered it stuck.
+             * 
+             * @return 10 times as much as eta if eta is available, else 24 hours.
+             * @see Executor#isLikelyStuck()
+             */
+            private long getLikelyStuckTime() {
+                Executor executor = build.getExecutor();
+                if (executor == null) {
+                    return TimeUnit2.HOURS.toMillis(24);
+                }
 
-				Executable executable = executor.getCurrentExecutable();
-				if (executable == null) {
-					return TimeUnit2.HOURS.toMillis(24);
-				}
+                Executable executable = executor.getCurrentExecutable();
+                if (executable == null) {
+                    return TimeUnit2.HOURS.toMillis(24);
+                }
 
-				long eta = Executables.getEstimatedDurationFor(executable);
-				if (eta >= 0) {
-					return eta * 10;
-				} else {
-					return TimeUnit2.HOURS.toMillis(24);
-				}
-			}
+                long eta = Executables.getEstimatedDurationFor(executable);
+                if (eta >= 0) {
+                    return eta * 10;
+                } else {
+                    return TimeUnit2.HOURS.toMillis(24);
+                }
+            }
 
             @Override
             public boolean tearDown(AbstractBuild build, BuildListener listener) throws IOException, InterruptedException {
@@ -135,11 +153,31 @@ public class BuildTimeoutWrapper extends BuildWrapper {
         }
 
         public String getDisplayName() {
-			return Messages.Descriptor_DisplayName();
+            return Messages.Descriptor_DisplayName();
         }
 
         public boolean isApplicable(AbstractProject<?, ?> item) {
             return true;
+        }
+
+        @Override
+        public BuildWrapper newInstance(StaplerRequest req, JSONObject formData) throws FormException {
+            BuildTimeoutWrapper instance = (BuildTimeoutWrapper) super.newInstance(req, formData);
+
+            // convert from submitted form value : radioBlock to property
+            Object strategy = formData.getJSONObject("strategy").get("value");
+            if ("fixationTimeStrategy".equals(strategy)) {
+                instance.fixationTimeStrategy = true;
+                instance.likelyStuckStrategy = false;
+            } else if ("likelyStuckStrategy".equals(strategy)) {
+                instance.fixationTimeStrategy = false;
+                instance.timeoutMinutes = 0;
+                instance.likelyStuckStrategy = true;
+            } else {
+                throw new IllegalArgumentException("submitted form value mismatch.");
+            }
+
+            return instance;
         }
 
     }
