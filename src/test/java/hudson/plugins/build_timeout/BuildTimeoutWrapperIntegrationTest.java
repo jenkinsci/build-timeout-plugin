044d829536668d2f24876419266ccba2236f09a8
Jesse Glick <jglick@cloudbees.com>
2013-11-19 18:22:56 -0500

    Making test a lot quicker.

--- src/test/java/hudson/plugins/build_timeout/BuildTimeoutWrapperIntegrationTest.java
+++ src/test/java/hudson/plugins/build_timeout/BuildTimeoutWrapperIntegrationTest.java
@@ -1,23 +1,32 @@
 package hudson.plugins.build_timeout;
 
+import hudson.model.Descriptor;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Result;
-import hudson.tasks.BuildWrapper;
-import hudson.tasks.test.AbstractTestResultAction;
+import hudson.model.Run;
 
 import org.jvnet.hudson.test.Bug;
 import org.jvnet.hudson.test.HudsonTestCase;
-import org.jvnet.hudson.test.recipes.LocalData;
+import org.jvnet.hudson.test.SleepBuilder;
 
 public class BuildTimeoutWrapperIntegrationTest extends HudsonTestCase {
 
 	@Bug(9203)
-	@LocalData
 	public void testIssue9203() throws Exception {
-		FreeStyleProject project = (FreeStyleProject) hudson.getItem("9203");
-		
+        BuildTimeoutWrapper.MINIMUM_TIMEOUT_MILLISECONDS = 0;
+		FreeStyleProject project = createFreeStyleProject();
+		project.getBuildWrappersList().add(new BuildTimeoutWrapper(new QuickBuildTimeOutStrategy(), true, false));
+        project.getBuildersList().add(new SleepBuilder(9999));
 		FreeStyleBuild build = project.scheduleBuild2(0).get();
 		assertBuildStatus(Result.FAILURE, build);
 	}
+    private static class QuickBuildTimeOutStrategy extends BuildTimeOutStrategy {
+        @Override public long getTimeOut(Run run) {
+            return 5000;
+        }
+        @Override public Descriptor<BuildTimeOutStrategy> getDescriptor() {
+            throw new UnsupportedOperationException();
+        }
+    }
 }
