915c8bf0b2e1b27c483f7afdcd70dd16ebf856d1
ikedam <devld@ikedam.jp>
2014-02-23 10:24:57 +0900

    [FIXED JENKINS-21919] Generalize actions performed in timeout with an extension point.

---
+++ src/main/java/hudson/plugins/build_timeout/BuildTimeOutOperation.java
@@ -0,0 +1,59 @@
+/*
+ * The MIT License
+ * 
+ * Copyright (c) 2014 IKEDA Yasuyuki
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package hudson.plugins.build_timeout;
+
+import jenkins.model.Jenkins;
+import hudson.ExtensionPoint;
+import hudson.model.AbstractBuild;
+import hudson.model.Action;
+import hudson.model.BuildListener;
+import hudson.model.Describable;
+
+/**
+ * Defines an operation performed when timeout occurs.
+ * They are called "Timeout Actions", but the class is BuildTimeOutOperation
+ * not to be confused with {@link Action}
+ */
+public abstract class BuildTimeOutOperation
+        implements ExtensionPoint, Describable<BuildTimeOutOperation> {
+    
+    /**
+     * Perform operation.
+     * 
+     * @param build             build timed out
+     * @param listener          build listener. can be used to print log.
+     * @param effectiveTimeout  timeout (milliseconds)
+     * @return false not to run subsequent operations, builders and publishers.
+     */
+    public abstract boolean perform(AbstractBuild<?,?> build, BuildListener listener, long effectiveTimeout);
+    
+    /**
+     * @return
+     * @see hudson.model.Describable#getDescriptor()
+     */
+    public BuildTimeOutOperationDescriptor getDescriptor() {
+        return (BuildTimeOutOperationDescriptor)Jenkins.getInstance().getDescriptorOrDie(getClass());
+    }
+}
---
+++ src/main/java/hudson/plugins/build_timeout/operations/AbortOperation.java
@@ -0,0 +1,78 @@
+/*
+ * The MIT License
+ * 
+ * Copyright (c) 2014 IKEDA Yasuyuki
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package hudson.plugins.build_timeout.operations;
+
+import static hudson.util.TimeUnit2.MILLISECONDS;
+import static hudson.util.TimeUnit2.MINUTES;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import hudson.Extension;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.model.Executor;
+import hudson.model.Result;
+import hudson.plugins.build_timeout.BuildTimeOutOperation;
+import hudson.plugins.build_timeout.BuildTimeOutOperationDescriptor;
+
+/**
+ * Abort the build.
+ */
+public class AbortOperation extends BuildTimeOutOperation {
+    @DataBoundConstructor
+    public AbortOperation(){
+    }
+    
+    /**
+     * @param build
+     * @param listener
+     * @param effectiveTimeout
+     * @return
+     * @see hudson.plugins.build_timeout.BuildTimeOutOperation#perform(hudson.model.AbstractBuild, hudson.model.BuildListener, long)
+     */
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, BuildListener listener, long effectiveTimeout) {
+        long effectiveTimeoutMinutes = MINUTES.convert(effectiveTimeout,MILLISECONDS);
+        // Use messages in hudson.plugins.build_timeout.Messages for historical reason.
+        listener.getLogger().println(hudson.plugins.build_timeout.Messages.Timeout_Message(
+                effectiveTimeoutMinutes,
+                hudson.plugins.build_timeout.Messages.Timeout_Aborted())
+        );
+        
+        Executor e = build.getExecutor();
+        if (e != null) {
+            e.interrupt(Result.ABORTED);
+        }
+        return true;
+    }
+    
+    @Extension(ordinal=100) // should be located at the top.
+    public static class DescriptorImpl extends BuildTimeOutOperationDescriptor {
+        @Override
+        public String getDisplayName() {
+            return Messages.AbortOperation_DisplayName();
+        }
+    }
+}
