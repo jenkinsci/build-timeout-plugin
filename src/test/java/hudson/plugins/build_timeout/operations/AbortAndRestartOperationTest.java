0c49e15509089b6fde7865fecc0d8db024465b94
Jochen-A-Fuerbacher <mail@jochen-fuerbacher.de>
2015-09-16 07:49:34 +0200

    Added / optimzed unit tests for Abort and Restart.

--- src/test/java/hudson/plugins/build_timeout/operations/AbortAndRestartOperationTest.java
+++ src/test/java/hudson/plugins/build_timeout/operations/AbortAndRestartOperationTest.java
@@ -1,27 +1,47 @@
+/*
+ * The MIT License
+ * 
+ * Copyright (c) 2015 Jochen A. Fuerbacher
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package hudson.plugins.build_timeout.operations;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.awt.List;
 import java.util.LinkedList;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.jvnet.hudson.test.JenkinsRule;
-import org.jvnet.hudson.test.recipes.LocalData;
 
 import hudson.model.Cause;
-import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Result;
 import hudson.plugins.build_timeout.*;
 import hudson.plugins.build_timeout.BuildTimeOutJenkinsRule;
-import hudson.plugins.build_timeout.BuildTimeOutOperationDescriptor;
 import hudson.plugins.build_timeout.BuildTimeoutWrapper;
 import hudson.plugins.build_timeout.impl.AbsoluteTimeOutStrategy;
-import hudson.tasks.Builder;
 import hudson.tasks.BatchFile;
+import hudson.tasks.Shell;
+
 
 public class AbortAndRestartOperationTest {
     
@@ -29,7 +49,7 @@ public class AbortAndRestartOperationTest {
     public BuildTimeOutJenkinsRule j = new BuildTimeOutJenkinsRule();
 
     @Test
-    public void testAbortAndRestart() throws Exception {
+    public void testAbortAndRestartOnce() throws Exception {
         
         FreeStyleProject testproject = j.createFreeStyleProject();
 
@@ -41,32 +61,96 @@ public class AbortAndRestartOperationTest {
         BuildTimeoutWrapper wrapper = new BuildTimeoutWrapper(strategy,list,"");
         testproject.getBuildWrappersList().add(wrapper);
         
-        
-        BatchFile builder = new BatchFile("ping -n 500 127.0.0.1 &amp;gt;nul");
-        testproject.getBuildersList().add(builder);
+        if(System.getProperty("os.name").contains("Windows")){
+            BatchFile builder = new BatchFile("ping -n 500 127.0.0.1 &amp;gt;nul");
+            testproject.getBuildersList().add(builder);
+        }else{
+            Shell builder = new Shell("ping -n 500 127.0.0.1 &amp;gt;nul");
+            testproject.getBuildersList().add(builder);
+        }
         
         assertTrue(testproject.getBuilds().size()==0);
                 
         
-  
+        testproject.scheduleBuild(new Cause.UserIdCause());
+        try{
+            Thread.sleep((3*60*1000)+20000);
+        }catch(InterruptedException e){
+            //Nothing todo here.
+        }
+        assertTrue(testproject.getFirstBuild() != null);
+        assertTrue(testproject.getFirstBuild() == testproject.getLastBuild());
+        assertEquals(testproject.getBuilds().size(), 1);
+        
+        try{
+            Thread.sleep((3*60*1000)+20000);
+        }catch(InterruptedException e){
+            //Nothing todo here.
+        }
+        assertTrue(testproject.getFirstBuild() != null);
+        assertTrue(testproject.getFirstBuild() != testproject.getLastBuild());
+        assertEquals(testproject.getBuilds().size(), 2);
+        
+        Thread.sleep(5000);
+        assertEquals(testproject.getBuilds().size(), 2);
+        assertEquals(Result.ABORTED, testproject.getFirstBuild().getResult());
+        assertEquals(Result.ABORTED, testproject.getLastBuild().getResult());
+    }
+    
+    @Test
+    public void testAbortAndRestartTwice() throws Exception {
+        
+        FreeStyleProject testproject = j.createFreeStyleProject();
 
-        FreeStyleBuild build = testproject.scheduleBuild2(0,new Cause.UserIdCause()).get();
-        j.assertBuildStatus(Result.ABORTED, build);
-//        Thread.sleep((3*60*1000)+20);
-//        assertTrue(testproject.getFirstBuild() != null);
-//        assertTrue(testproject.getFirstBuild().getResult() != null);
-//        assertEquals(testproject.getFirstBuild().getResult(), Result.ABORTED);
+        AbsoluteTimeOutStrategy strategy = new AbsoluteTimeOutStrategy(Integer.toString(3)); //timeoutMinutes
+        AbortAndRestartOperation operation = new AbortAndRestartOperation(2); //Number of restarts
+        LinkedList<BuildTimeOutOperation> list = new LinkedList<BuildTimeOutOperation>();
+        list.add(operation);
         
-        //j.assertBuildStatus(Result.ABORTED,testproject.scheduleBuild2(0, new Cause.UserIdCause()).get());
+        BuildTimeoutWrapper wrapper = new BuildTimeoutWrapper(strategy,list,"");
+        testproject.getBuildWrappersList().add(wrapper);
         
-        //assertBuildStatus(Result.ABORTED, testproject.scheduleBuild2(0, new Cause.UserIdCause()).get());
+        if(System.getProperty("os.name").contains("Windows")){
+            BatchFile builder = new BatchFile("ping -n 500 127.0.0.1 &amp;gt;nul");
+            testproject.getBuildersList().add(builder);
+        }else{
+            Shell builder = new Shell("ping -n 500 127.0.0.1 &amp;gt;nul");
+            testproject.getBuildersList().add(builder);
+        }
         
-//        Thread.sleep(150000);
-//        assertTrue(testproject.getFirstBuild() != null);
-//        assertTrue(testproject.getFirstBuild().getResult() != null);
-//        assertEquals(testproject.getFirstBuild().getResult(), Result.ABORTED);
-        //Thread.sleep(70000);
-//        assertEquals(testproject.getLastBuild().getResult(), Result.ABORTED);
-//        assertEquals(testproject.getBuilds().size(), 2);
+        assertTrue(testproject.getBuilds().size()==0);
+                
+        
+        testproject.scheduleBuild(new Cause.UserIdCause());
+        try{
+            Thread.sleep((3*60*1000)+20000);
+        }catch(InterruptedException e){
+            //Nothing todo here.
+        }
+        assertTrue(testproject.getFirstBuild() != null);
+        assertTrue(testproject.getFirstBuild() == testproject.getLastBuild());
+        assertEquals(testproject.getBuilds().size(), 1);
+        
+        try{
+            Thread.sleep((3*60*1000)+20000);
+        }catch(InterruptedException e){
+            //Nothing todo here.
+        }
+        
+        try{
+            Thread.sleep((3*60*1000)+20000);
+        }catch(InterruptedException e){
+            //Nothing todo here.
+        }
+        
+        assertTrue(testproject.getFirstBuild() != null);
+        assertTrue(testproject.getFirstBuild() != testproject.getLastBuild());
+        assertEquals(testproject.getBuilds().size(), 3);
+        
+        Thread.sleep(5000);
+        assertEquals(testproject.getBuilds().size(), 3);
+        assertEquals(Result.ABORTED, testproject.getBuildByNumber(1).getResult());
+        assertEquals(Result.ABORTED, testproject.getBuildByNumber(2).getResult());
+        assertEquals(Result.ABORTED, testproject.getBuildByNumber(3).getResult());
     }
 }
