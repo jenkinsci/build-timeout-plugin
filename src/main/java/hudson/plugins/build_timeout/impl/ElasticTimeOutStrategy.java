ea2bc002e9c1895cd68bbed8128d33ea49f8714d
Felix Belzunce Arcos <Felix@Felixs-MacBook-Pro.local>
2015-09-21 14:12:55 +0200

    timeoutMinutesElasticDefault should be used until numberOfBuilds is not reached out

--- src/main/java/hudson/plugins/build_timeout/impl/ElasticTimeOutStrategy.java
+++ src/main/java/hudson/plugins/build_timeout/impl/ElasticTimeOutStrategy.java
@@ -95,7 +95,7 @@ public class ElasticTimeOutStrategy extends BuildTimeOutStrategy {
         }
 
 
-        return nonFailingBuilds > 0 ? ((double)durationSum) / nonFailingBuilds : 0;
+        return nonFailingBuilds >=numberOfBuilds ? ((double)durationSum) / nonFailingBuilds : 0;
     }
 
     public Descriptor<BuildTimeOutStrategy> getDescriptor() {
