c9542adce70b6a48bbaf3e05b2163fd1fc73b36b
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-10-02 17:12:12 +0100

    Revert "Add Action to persistent the timeout reason in the build"
    This reverts commit 6c635f2a50d76149fe08c68326b470a70e7f1d0d.

--- src/main/java/hudson/plugins/build_timeout/BuildTimeOutOperation.java
+++ src/main/java/hudson/plugins/build_timeout/BuildTimeOutOperation.java
@@ -1,18 +1,18 @@
 /*
  * The MIT License
- *
+ * 
  * Copyright (c) 2014 IKEDA Yasuyuki
- *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -40,29 +40,21 @@ import javax.annotation.Nonnull;
  */
 public abstract class BuildTimeOutOperation
         implements ExtensionPoint, Describable<BuildTimeOutOperation> {
-
+    
     /**
      * Perform operation.
-     *
+     * 
      * @param build             build timed out
      * @param listener          build listener. can be used to print log.
      * @param effectiveTimeout  timeout (milliseconds)
      * @return false not to run subsequent operations. It also mark the build as failure.
      */
     public abstract boolean perform(@Nonnull AbstractBuild<?,?> build, @Nonnull BuildListener listener, long effectiveTimeout);
-
+    
     /**
      * @see hudson.model.Describable#getDescriptor()
      */
     public BuildTimeOutOperationDescriptor getDescriptor() {
         return (BuildTimeOutOperationDescriptor)Jenkins.getActiveInstance().getDescriptorOrDie(getClass());
     }
-
-	public void addAction(@Nonnull AbstractBuild<?,?> build, @Nonnull String reason) {
-		BuildTimeOutAction buildTimeoutAction = build.getAction(BuildTimeOutAction.class);
-		if(buildTimeoutAction == null) {
-		    buildTimeoutAction = new BuildTimeOutAction(reason);
-		}
-	    build.addAction(buildTimeoutAction);
-	}
 }
--- src/main/java/hudson/plugins/build_timeout/operations/FailOperation.java
+++ src/main/java/hudson/plugins/build_timeout/operations/FailOperation.java
@@ -1,18 +1,18 @@
 /*
  * The MIT License
- *
+ * 
  * Copyright (c) 2014 IKEDA Yasuyuki
- *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -44,7 +44,7 @@ public class FailOperation extends BuildTimeOutOperation {
     @DataBoundConstructor
     public FailOperation() {
     }
-
+    
     /**
      * @see hudson.plugins.build_timeout.BuildTimeOutOperation#perform(hudson.model.AbstractBuild, hudson.model.BuildListener, long)
      */
@@ -56,17 +56,15 @@ public class FailOperation extends BuildTimeOutOperation {
                 effectiveTimeoutMinutes,
                 hudson.plugins.build_timeout.Messages.Timeout_Failed())
         );
-
-        addAction(build, "FailOperation");
-
+        
         Executor e = build.getExecutor();
         if (e != null) {
             e.interrupt(Result.FAILURE);
         }
-
+        
         return true;
     }
-
+    
     @Extension(ordinal=50) // should be located at the second.
     public static class DescriptorImpl extends BuildTimeOutOperationDescriptor {
         @Override
