jenkinsci/build-timeout-plugin

commit a14747ab52637089fbb6a61aa7322cfc40b5c842

Let individual jobs overwrite the global timeout (#95)
* Add checkbox to global configuration to let individual jobs overwrite the global timeout

Co-authored-by: simonsymhoven <simon.symhove@hm.edu>

src/main/java/hudson/plugins/build_timeout/global/GlobalTimeOutConfiguration.java
@@ -3,11 +3,11 @@ package hudson.plugins.build_timeout.global;
 import hudson.Extension;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
-import hudson.plugins.build_timeout.BuildTimeOutOperation;
-import hudson.plugins.build_timeout.BuildTimeOutOperationDescriptor;
-import hudson.plugins.build_timeout.BuildTimeOutStrategy;
-import hudson.plugins.build_timeout.BuildTimeOutStrategyDescriptor;
+import hudson.model.FreeStyleProject;
+import hudson.model.Project;
+import hudson.plugins.build_timeout.*;
 import hudson.plugins.build_timeout.operations.AbortOperation;
+import hudson.tasks.Builder;
 import jenkins.model.GlobalConfiguration;
 import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
@@ -32,6 +32,7 @@ public class GlobalTimeOutConfiguration extends GlobalConfiguration implements T
     private transient Jenkins jenkins;
     private BuildTimeOutStrategy strategy;
     private List<BuildTimeOutOperation> operations;
+    private boolean overwriteable;
 
     public GlobalTimeOutConfiguration() {
         load();
@@ -60,6 +61,7 @@ public class GlobalTimeOutConfiguration extends GlobalConfiguration implements T
         if (settings.isNullObject()) {
             strategy = null;
             operations = null;
+            overwriteable = false;
             log.info("global timeout has been cleared");
         } else {
             req.bindJSON(this, settings);
@@ -83,7 +85,10 @@ public class GlobalTimeOutConfiguration extends GlobalConfiguration implements T
     @Override
     public Optional<Duration> timeOutFor(AbstractBuild<?, ?> build, BuildListener listener) {
         try {
-            if (strategy == null) {
+            List<Builder> builders = ((Project<?, ?>) build.getProject()).getBuilders();
+            Optional<Builder> timeoutBuildStep = builders.stream().filter(builder -> builder instanceof BuildStepWithTimeout).findAny();
+
+            if (strategy == null || (timeoutBuildStep.isPresent() && isOverwriteable())) {
                 return Optional.empty();
             }
             return Optional.of(Duration.ofMillis(strategy.getTimeOut(build, listener)));
@@ -95,6 +100,15 @@ public class GlobalTimeOutConfiguration extends GlobalConfiguration implements T
 
     public boolean isEnabled() {
         return strategy != null;
+    }
+
+    public boolean isOverwriteable() {
+        return overwriteable;
+    }
+    
+    
+    public void setOverwriteable(boolean overwriteable) {
+        this.overwriteable = overwriteable;
     }
 
     @Override

src/main/resources/hudson/plugins/build_timeout/global/GlobalTimeOutConfiguration/config.jelly
@@ -10,7 +10,9 @@
               <f:hetero-list name="operations" hasHeader="true"
                 descriptors="${instance.allOperations}" items="${instance.operations}"
                 addCaption="${%Add action}" />
-            </f:entry>
+        </f:entry>
+        <f:optionalBlock field="overwriteable" name="overwriteable" inline="true" 
+                title="Are individual jobs allowed to overwrite this global timeout?" checked="${instance.overwriteable}"/>
     </f:optionalBlock>
 
 </f:section>

commit 32dcfb13971f4f712cac0cd8de89fa3d4e354230

Fix bug introduced in PR #95

src/main/java/hudson/plugins/build_timeout/global/GlobalTimeOutConfiguration.java
@@ -3,9 +3,12 @@ package hudson.plugins.build_timeout.global;
 import hudson.Extension;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
-import hudson.model.FreeStyleProject;
 import hudson.model.Project;
-import hudson.plugins.build_timeout.*;
+import hudson.plugins.build_timeout.BuildTimeOutOperation;
+import hudson.plugins.build_timeout.BuildTimeOutOperationDescriptor;
+import hudson.plugins.build_timeout.BuildTimeOutStrategy;
+import hudson.plugins.build_timeout.BuildTimeOutStrategyDescriptor;
+import hudson.plugins.build_timeout.BuildStepWithTimeout;
 import hudson.plugins.build_timeout.operations.AbortOperation;
 import hudson.tasks.Builder;
 import jenkins.model.GlobalConfiguration;
@@ -92,6 +95,18 @@ public class GlobalTimeOutConfiguration extends GlobalConfiguration implements T
                 return Optional.empty();
             }
             return Optional.of(Duration.ofMillis(strategy.getTimeOut(build, listener)));
+
+        } catch (ClassCastException e) {
+            log.log(WARNING, e, () -> String.format("%s cannot allow individual jobs to overwrite timeout due to ClassCastException", build.getExternalizableId()));
+            if (strategy == null) {
+                return Optional.empty();
+            }
+            try {
+                return Optional.of(Duration.ofMillis(strategy.getTimeOut(build, listener)));
+            } catch (InterruptedException | MacroEvaluationException | IOException ex) {
+                log.log(WARNING, ex, () -> String.format("%s failed to determine time out", build.getExternalizableId()));
+                return Optional.empty();
+            }
         } catch (InterruptedException | MacroEvaluationException | IOException e) {
             log.log(WARNING, e, () -> String.format("%s failed to determine time out", build.getExternalizableId()));
             return Optional.empty();